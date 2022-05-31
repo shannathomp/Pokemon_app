844fc19c46c5b1ac6d4e95fe5ed1355fc34c31e7
unknown <shannathomP@yahoo.com>
2022-05-31 14:50:30 -0400

    Connected my database

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const pokemon = require('./models/pokemon')
 const app = express()
 const port = 3000
+// app.use((req,res,next) =>{
+//     console.log(`running middleware function!!!`);
+//     next()// going to the next middleware or to the response
+//     })
+
+
+// const getData = require('./models/pokemon')
+// const pokeData = getData()
+
 
 app.set('view engine', 'ejs')
 app.set('views', './Views')
@@ -8,6 +18,14 @@ app.set('views', './Views')
 app.get('/', (req,res) => {
     res.send('Welcome to the Pokemon App!')
 })
+
+app.get('/pokemon', (req,res) => {
+    res.render('pokemon')
+})
+
+// app.get('/pokemon', (req,res) => {
+//     res.render('index', {data: pokemon})
+// })
 
 app.listen(port, () => {
     console.log(`running`);
